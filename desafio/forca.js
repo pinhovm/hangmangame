e428bcfaa59a9350be0871235f9c07c4a1f506ad
pinhovm <menesesvictor56@gmail.com>
2022-07-24 16:40:02 -0300

    Desafio Concluido

--- desafio/forca.js
+++ desafio/forca.js
@@ -1,16 +1,54 @@
 class Forca {
+  constructor(palavraCorreta) {
+    this.palavraCorreta = palavraCorreta;
+    this.letrasChutadas = []
+    this.vidas = 6;
+    this.palavra = (() => {
+      let underline = []
+      for (let i = 0; i < palavraCorreta.length; i++) {
+        underline.push("_")
+      }
+      return underline
+    })()
+    
+  }
 
-  chutar(letra) { }
+  chutar(letra) {
+    if (letra.length > 1) {
+    } else {
+      if (this.letrasChutadas.includes(letra, 0)) {
+      } else {
+        this.letrasChutadas.push(letra)
+        if (this.palavraCorreta.includes(letra, 0)) {
+          for (let i = 0; i < this.palavraCorreta.length; i++) {
+            if (this.palavraCorreta[i] === letra) {
+              this.palavra[i] = letra
+            }
+          }
+        } else {
+          this.vidas--;
+        }
+      }
+    }
+  }
 
-  buscarEstado() { return ""; } // Possiveis valores: "perdeu", "aguardando chute" ou "ganhou"
+  buscarEstado() {
+    if (this.vidas === 0) {
+      return "perdeu"
+    } else if (this.palavra.join('') === this.palavraCorreta) {
+      return "ganhou"
+    } else {
+      return "aguardando chute"
+    }
+  }
 
   buscarDadosDoJogo() {
-      return {
-          letrasChutadas: [], // Deve conter todas as letras chutadas
-          vidas: 6, // Quantidade de vidas restantes
-          palavra: [] // Deve ser um array com as letras que já foram acertadas ou o valor "_" para as letras não identificadas
-      }
+    return {
+      letrasChutadas: this.letrasChutadas, // Deve conter todas as letras chutadas
+      vidas: this.vidas, // Quantidade de vidas restantes
+      palavra: this.palavra // Deve ser um array com as letras que já foram acertadas ou o valor "_" para as letras não identificadas
+    }
   }
 }
 
-module.exports = Forca;
+module.exports = Forca
